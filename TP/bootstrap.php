4dbe28401dfd21b6686223cb7856c15ad41e83c4
Sergio Rebolledo <sergio.rebolledo@est.fi.uncoma.edu.ar>
2025-11-08 06:36:57 -0300

    reinicio tp 5 y final

---
+++ TP/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager;
+
+// Definimos la configuración para el driver de base de datos que usemos (MySQL en este caso).
+$config = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'some_database',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => ''
+];
+
+// La clase `Manager` se encarga de todo, así que la creamos y usamos `addConnection` para pasarle la configuración y seleccionar el driver.
+$capsule = new Manager();
+
+$capsule->addConnection($config, 'mysql');
+
+// Hacemos que la instancia de Manager esté disponible globalmente
+$capsule->setAsGlobal();
+
+// Inicializamos `bootEloquent` para poder usar `Models` globalmente.
+$capsule->bootEloquent();
